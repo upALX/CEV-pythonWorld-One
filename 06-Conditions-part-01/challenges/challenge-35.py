565cc9014710316e1c96c8d9494fc82392b83d75
Alexandre <alexandreaddres@gmail.com>
2021-10-29 00:26:15 -0300

    Challenge 05 more one function done!

--- 06-Conditions-part-01/challenges/challenge-35.py
+++ 06-Conditions-part-01/challenges/challenge-35.py
@@ -1,10 +1,18 @@
 # Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
 
+from datetime import date
+
 print('---ANO BISSEXTO---')
-yearBi = int(input('Olá, informe um ano para que eu verifique se ele é bissexto: '))
-yearBiCalc = yearBi % 100 == 0
-yearBiNot = (yearBiCalc % 400) == 0
-if yearBiNot:
-    print('Bissexto!')
+year = int(input('Olá, informe um ano para que eu verifique se ele é bissexto - informe "0" para o ano atual: '))
+
+if year == 0:
+    year = date.today().year
+
+yearBi = year % 4 == 0 #Cálculo do ano bissexto!
+yearBiNot = yearBi % 100 == 0 #Cálculo do ano quando não é bissexto
+yearBiException = (yearBiNot % 400) == 0 # Cálculo do ano bissexto com exceção
+
+if yearBiException:
+    print('O ano {} que você informou é bissexto!'.format(year))
 else:
-    print('Não é bissexto!')
+    print('O ano {} que você informou não é bissexto!'.format(year))
