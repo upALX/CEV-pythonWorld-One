30e6aa9404c7ae615c80fe8f2a53d9f3a0e7168a
José Alexandre <59508016+upALX@users.noreply.github.com>
2021-11-10 16:52:39 -0300

    Update description.mk

--- 02-Data-Processing-And-Accounts/description.mk
+++ 02-Data-Processing-And-Accounts/description.mk
@@ -1,6 +1,4 @@
-#Enunciados | Aula 06 e Desafios 3 e 4
-
-##Desafios 3 e 4
+## Desafios 3 e 4 | Aula 06
 - Crie um programa que leia dois números e mostre a soma entre eles (utilizando os valores primitivos).
 - Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
 
